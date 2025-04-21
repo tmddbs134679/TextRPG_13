c97ddc7ae32361fb0a87c9c3a6ef96b7d461cd47
나연 <99782442+N-Y-P@users.noreply.github.com>
2025-04-21 20:54:56 +0900

    add : Enums, Monster, MonsterStatement
    Enums에 몬스터 이름 목록 추가
    Monster에 Dictionary 추가
    MonsterStatement에 몬스터 정보 추가

--- TextRPG_13/Monster.cs
+++ TextRPG_13/Monster.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace TextRPG_13
 {
-    internal class Monster
+    public class Monster
     {
+        public MonsterType Type { get; }
+        public MonsterStatement Stats { get; }
+
+        private static readonly Dictionary<MonsterType, MonsterStatement> monsterPresets =
+            new Dictionary<MonsterType, MonsterStatement>
+            {
+                {MonsterType.MINION, new MonsterStatement("미니언", 2, 15, 5 ) },
+                { MonsterType.VOIDWORM,    new MonsterStatement("공허충", 5, 200, 20) },
+                { MonsterType.SIEGEMINION, new MonsterStatement("대포미니언", 3, 100, 10) },
+            };
+        private static readonly Random random = new Random();
+
     }
 }
