9cf33834d67b378df139d0529466c22eb508d199
rmqrnjs6 <kkw201030@gmail.com>
2025-04-21 20:59:14 +0900

    Update UIManager.cs
    게임 시작 화면

--- TextRPG_13/UIManager.cs
+++ TextRPG_13/UIManager.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace TextRPG_13
 {
-    internal class UIManager
+    public class UIManager
     {
+        public void GameLobby()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("스파르타 마을에 오신 여러분, 환영합니다.\n" +
+                                      "이제 전투를 시작할 수 있습니다.\n\n" +
+                                      "1. 상태 보기\n" +
+                                      "2. 전투 시작\n" +
+                                      "3. 회복 아이템\n\n" +
+                                      "원하시는 행동을 입력해주세요.\n" +
+                                      ">> "); int inp2 = int.Parse(Console.ReadLine());
+
+                        switch ((Lobbychoice)inp2)
+                        {
+                            case Lobbychoice.PlayerStat:
+                                PlayerStat();
+                                break;
+                            case Lobbychoice.Denjeon:
+
+                                break;
+                            case Lobbychoice.Potion:
+
+                                break;
+                        } 
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    GameLobby();
+                }
+            }
+        }
     }
 }
