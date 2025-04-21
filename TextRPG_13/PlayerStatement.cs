84c8a7ea2d734c23a29a460bed9ef0be06b7dffb
rmqrnjs6 <58199377+rmqrnjs6@users.noreply.github.com>
2025-04-21 20:23:55 +0900

    Update PlayerStatement.cs
    상태 보기 창 구현

--- TextRPG_13/PlayerStatement.cs
+++ TextRPG_13/PlayerStatement.cs
@@ -8,5 +8,25 @@ namespace TextRPG_13
 {
     internal class PlayerStatement
     {
+            public void PlayerStat()
+    {
+        Console.WriteLine("상태 보기\n");
+        Console.WriteLine("캐릭터의 정보가 표시됩니다.\n\n");
+        Console.WriteLine($"Lv. {Level}\n" +
+                          $"{Name}  ( {} )\n" +
+                          $"공격력 : {}\n" +
+                          $"방어력 : {}\n" +
+                          $"체 력 : {} \n" +
+                          $"Gold : {} \n\n" +
+                          $"0. 나가기\n\n" +
+                          $"원하시는 행동을 입력해주세요.\n" +
+                          $">>"); int inp = int.Parse(Console.ReadLine());
+
+        if (inp == 0)
+        {
+            Thread.Sleep(1000);
+            Console.Clear();
+
+        }
     }
 }
