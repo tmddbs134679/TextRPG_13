61cf19483326cce9791713658cb6db378d98cd54
rmqrnjs6 <kkw201030@gmail.com>
2025-04-22 10:21:07 +0900

    상태 보기, 시작 화면
    몇가지 카멜 표기법 수정,
    컬러 설정

--- TextRPG_13/PlayerStatement.cs
+++ TextRPG_13/PlayerStatement.cs
@@ -11,23 +11,32 @@ namespace TextRPG_13
         // {}은 Player에서 따로 할당이 됐을경우 주입
         public void PlayerStat()
         {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("상태 보기\n");
+            Console.ResetColor();
             Console.WriteLine("캐릭터의 정보가 표시됩니다.\n\n");
-            Console.WriteLine($"Lv. {}\n" +
-                              $"{}  ( {} )\n" +
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("Lv. ");
+            Console.ResetColor();
+            Console.WriteLine($"{}\n");
+            Console.WriteLine($"{}  ( {} )\n" +
                               $"공격력 : {}\n" +
                               $"방어력 : {}\n" +
                               $"체 력 : {} \n" +
                               $"Gold : {} \n\n" +
                               $"0. 나가기\n\n" +
-                              $"원하시는 행동을 입력해주세요.\n" +
-                              $">>"); int inp = int.Parse(Console.ReadLine());
+                              $"원하시는 행동을 입력해주세요.");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(">> ");
+            Console.ResetColor();
+            int inp = int.Parse(Console.ReadLine());
 
             if (inp == 0)
             {
                 Thread.Sleep(1000);
                 Console.Clear();
-                //게임 로비로 나가기
+                Gamelobby();
             }
         }
     }
--- TextRPG_13/UIManager.cs
+++ TextRPG_13/UIManager.cs
@@ -8,32 +8,51 @@ namespace TextRPG_13
 {
     public class UIManager
     {
-        public void GameLobby()
+        public void Gamelobby()
         {
             while (true)
             {
                 try
                 {
                     Console.WriteLine("스파르타 마을에 오신 여러분, 환영합니다.\n" +
-                                      "이제 전투를 시작할 수 있습니다.\n\n" +
-                                      "1. 상태 보기\n" +
-                                      "2. 전투 시작\n" +
-                                      "3. 회복 아이템\n\n" +
-                                      "원하시는 행동을 입력해주세요.\n" +
-                                      ">> "); int inp2 = int.Parse(Console.ReadLine());
+                                      "이제 전투를 시작할 수 있습니다.\n\n");
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Write("1. ");
+                    Console.ResetColor();
+                    Console.WriteLine("상태 보기");
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Write("2. ");
+                    Console.ResetColor();
+                    Console.WriteLine("전투 시작");
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Write("3. ");
+                    Console.ResetColor();
+                    Console.WriteLine("회복 아이템\n\n");
+                    Console.WriteLine("원하시는 행동을 입력해주세요.\n");
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write(">> ");
+                    Console.ResetColor();
+                    int inp2 = int.Parse(Console.ReadLine());
+                    switch ((Lobbychoice)inp2)
+                    {
+                        case Lobbychoice.PlayerStat:
+                            PlayerStat();
+                            break;
+                        case Lobbychoice.Denjeon:
+                            Thread.Sleep(5000);
+                            Console.Clear();
+                            break;
+                        case Lobbychoice.Potion:
 
-                        switch ((Lobbychoice)inp2)
-                        {
-                            case Lobbychoice.PlayerStat:
-                                PlayerStat();
-                                break;
-                            case Lobbychoice.Denjeon:
-
-                                break;
-                            case Lobbychoice.Potion:
-
-                                break;
-                        } 
+                            break;
+                    }
+                    if (inp2 >= 1 || inp2 <= 3)
+                    {
+                        Console.WriteLine("1~3의 맞는 행동을 입력해주세요.");
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                        Gamelobby();
+                    }
                 }
                 catch (Exception e)
                 {
@@ -41,7 +60,7 @@ namespace TextRPG_13
                     Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
                     Thread.Sleep(1000);
                     Console.Clear();
-                    GameLobby();
+                    Gamelobby();
                 }
             }
         }
