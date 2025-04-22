1fc393f4a58c6a987b1abfb2735cf4887485beee
rmqrnjs6 <kkw201030@gmail.com>
2025-04-22 11:31:56 +0900

    add : 상태보기 창
    인벤토리에서 장착한 아이템 값을 합산하여 상태 표시 창에서
    합산된 값, 장착한 아이템의 공격,방어력 표시하는 로직 추가

--- TextRPG_13/PlayerStatement.cs
+++ TextRPG_13/PlayerStatement.cs
@@ -12,6 +12,13 @@ namespace TextRPG_13
         public void PlayerStat()
         {
             Console.Clear();
+
+            int Bonusoff = Splayer.InventoryItems.Where(i => i.IsEquipped).Sum(i => i.);
+
+            // 인벤토리에서 장착한 아이템을 Bonusoff,Bonusdf 에 선언하여 각각 공격력,방어력에 합산하여 상태 표시 창에서 합산된 값 표시.
+
+            int Bonusdf = Splayer.InventoryItems.Where(i => i.IsEquipped).Sum(i => i.);
+
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("상태 보기\n");
             Console.ResetColor();
@@ -27,12 +34,18 @@ namespace TextRPG_13
             Console.Write($"공격력 : ");
 
             Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.WriteLine($"{}");
+            Console.WriteLine($"{} (+{Bonusoff})");
+            // 인벤토리 추가 될 시 인벤토리에서 장착한 아이템을 Bonusoff에 장비에 따른 공격력 추가 및 기본 공격력에 합산,
+            // 기본 공격력엔 Bonusoff가 합산된 전체값 표기
+            // Bonus 에는 장착한 장비유형에 따른 값 표시 (공격력 : 6 일 경우 bonusoff 에는 그 장비의 공격력 6 추가)
             Console.ResetColor();
 
             Console.Write("방어력 : ");
             Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.WriteLine($"{}");
+            Console.WriteLine($"{} (+{BonusDf})");
+            // 인벤토리 추가 될 시 인벤토리에서 장착한 아이템을 Bonusdf에 장비에 따른 공격력 추가 및 기본 방어력에 합산,
+            // 기본 공격력엔 Bonusdf가 합산된 전체값 표기
+            // Bonus 에는 장착한 장비유형에 따른 값 표시 (방어력 : 6 일 경우 bonusoff 에는 그 장비의 방어력 6 추가) 
             Console.ResetColor();
 
             Console.Write("체 력 : ");
