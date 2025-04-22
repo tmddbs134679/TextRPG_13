771f54e69f25265b029ed44dd87153868df08d8a
rmqrnjs6 <kkw201030@gmail.com>
2025-04-22 18:46:51 +0900

    dd

--- TextRPG_13/MonsterStatement.cs
+++ TextRPG_13/MonsterStatement.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace TextRPG_13
 {
-    internal class MonsterStatement
+    public class MonsterStatement
     {
+        public string monsterName { get; set; }
+        public int Lv { get; set; }
+        public int monsterHP { get; set; }
+        public int monsterATK { get; set; }
+
+        public MonsterStatement(string name, int lv, int hp, int atk)
+        {
+            monsterName = name;
+            Lv = lv;
+            monsterHP = hp;
+            monsterATK = atk;
+        }
+        //몬스터가 중복 생성될 때 
+        public MonsterStatement Clone()
+        {
+            return new MonsterStatement(
+                monsterName,
+                Lv,
+                monsterHP,
+                monsterATK
+            );
+        }
+
+
+
+
     }
 }
