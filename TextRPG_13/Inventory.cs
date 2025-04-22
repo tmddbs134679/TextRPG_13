82e6d65e88dc55173d2023c5f35e6c95baec7146
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-04-22 18:07:27 +0900

    Add : 인벤토리, 아이템, 스킬

---
+++ TextRPG_13/Inventory.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_13
+{
+    public class Inventory
+    {
+        private List<Item> items;
+        public int Count => items.Count;
+        public bool IsEmpty => !items.Any();
+
+        public Inventory()
+        {
+            items = new List<Item>();
+        }
+
+
+        public List<Item> GetEquippedItems()
+        {
+            return items.Where(i => i.IsEquipped).ToList();
+        }
+
+        //public List<Item> GetItems()
+        //{
+        //    return items;
+        //}
+
+        //public void AddItem(Item item, ITEMTYPE type)
+        //{
+        //    item.Type = type;
+        //    items.Add(item);
+        //}
+
+        public void RemoveItem(Item item)
+        {
+            items.Remove(item);
+        }
+
+        public void EquipItem(Item itemEquip)
+        {
+            // 이미 장착 중인 아이템을 다시 선택한 경우 → 해제
+            if (itemEquip.IsEquipped)
+            {
+                itemEquip.IsEquipped = false;
+                return;
+            }
+
+            //foreach (var item in items)
+            //{
+            //    if (item.Type == itemEquip.Type && item.IsEquipped)
+            //    {
+            //        item.IsEquipped = false;
+            //        break;
+            //    }
+            //}
+
+            itemEquip.IsEquipped = true;
+
+        }
+
+
+    }
+}
---
+++ TextRPG_13/Item.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_13
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        //public ITEMTYPE Type { get; set; }
+        public int Power { get; set; }
+
+        public int Defense { get; set; }
+        public int PurchasePrice { get; set; }
+        public int SellPrice { get; set; }
+        public string Description { get; set; }
+        public bool IsEquipped { get; set; } = false;
+
+        //public bool IsEquipable => Type == ITEMTYPE.WEAPON || Type == ITEMTYPE.ARMOR;
+        //public bool IsConsumable => Type == ITEMTYPE.POTION;
+
+        //public Item(string name, ITEMTYPE type, int power, int defense, int purchaseprice, string description)
+        //{
+        //    Name = name;
+        //    //Type = type;
+        //    Power = power;
+        //    Defense = defense;
+        //    PurchasePrice = purchaseprice;
+        //    SellPrice = (int)((int)PurchasePrice * 0.7);
+        //    Description = description;
+        //}
+
+    }
+}
