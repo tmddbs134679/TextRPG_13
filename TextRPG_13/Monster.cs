ce20a5a9ac5ffeb6d75b39fb2b6893428fd6f999
나연 <99782442+N-Y-P@users.noreply.github.com>
2025-04-22 15:09:04 +0900

    add : monster 주석추가

--- TextRPG_13/Monster.cs
+++ TextRPG_13/Monster.cs
@@ -41,7 +41,7 @@ namespace TextRPG_13
 
         //Monster.MonsterRandomSpawn();한줄로 다른 곳에서 몬스터 랜덤 스폰 메서드 사용가능
         private static List<Monster> currentWave; 
-        public static IReadOnlyList<Monster> CurrentWave => currentWave;
+        public static IReadOnlyList<Monster> CurrentWave => currentWave; //다른 곳에서 몬스터 생성 못하게 읽게만
         //var monsterInfo = Monster.CurrentWave[0];으로 랜덤 생성된 몬스터의 정보보기 가능
         //예) {monsterInfo.Stats.monsterHP} 첫번째에 저장된 몬스터의 체력 정보보기
         public static void MonsterRandomSpawn()
@@ -50,15 +50,17 @@ namespace TextRPG_13
 
             // 1~4마리 랜덤 등장
             int count = random.Next(1, 5);
-            if (currentWave == null)
+            if (currentWave == null)//아직 리스트에 몬스터 정보가 없다면
             {
-                currentWave = new List<Monster>(capacity: count);
+                // 리스트 용량을 count(랜덤값)만큼 미리 예약
+                currentWave = new List<Monster>(capacity: count);//그냥 count로 해도 무방. 저건 성능용
                 for (int i = 0; i < count; i++)
                 {
                     currentWave.Add(CreateRandom());
                 }
             }
 
+            //다음에 MonsterRandomSpawn()를 다시 호출시 앞에 if문 건너뛰고 여기로
             foreach (var m in currentWave)
             {
                 ui.PrintRandomMonster(m);
