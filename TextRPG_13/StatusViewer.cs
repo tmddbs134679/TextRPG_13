fb76844e2008c2ee1ce0066b084b8636895d883a
rmqrnjs6 <kkw201030@gmail.com>
2025-04-23 19:46:44 +0900

    add : 캐릭터 선택 후 인트로 화면 추가

--- TextRPG_13/StatusViewer.cs
+++ TextRPG_13/StatusViewer.cs
@@ -39,7 +39,7 @@ namespace TextRPG_13
 
                 if (int.TryParse(Console.ReadLine(), out int choice) && choice == 0)
                 {
-                    Console.Write("로비로 이동 중");
+                    Console.Write("\n로비로 이동 중");
                     for (int i = 0; i < 5; i++)
                     {
                         Thread.Sleep(500);
